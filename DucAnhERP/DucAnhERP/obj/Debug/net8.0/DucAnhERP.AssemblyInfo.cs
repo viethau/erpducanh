99111553e5ae706e2fed2b8d84fbc3b7f7d29894
anhquangminh <qminh97ictu@gmail.com>
2024-11-26 16:53:48 +0700

    update 26/11/2024

--- DucAnhERP/DucAnhERP/obj/Debug/net8.0/DucAnhERP.AssemblyInfo.cs
+++ DucAnhERP/DucAnhERP/obj/Debug/net8.0/DucAnhERP.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DucAnhERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac7caaf6e79b381748006e485be466be9d83b45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d65b610a611496f4fc8c27dd3695f6870428ed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DucAnhERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DucAnhERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
